2f517613b1188ee87b8813d25d1a5a019a55f273
NAGARJUNAGALLA <nagarjunagalla@gmail.com>
2026-07-22 21:43:12 +0530

    Upgrade Google Services plugin to version 4.4.2
    Updated Google Services plugin version to 4.4.2 to fix dependency mutation crash.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    // Updated to 4.4.2 to fix the dependency mutation crash on Gradle 8.6+
+    id("com.google.gms.google-services") version "4.4.2" apply false 
 }
